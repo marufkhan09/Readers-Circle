1c7513ce30b52003a489e7426689d92cdafd8034
maruufkhan09 <maruf1609@gmail.com>
2025-01-03 18:30:09 +0600

    gradle issue fixed

--- lib/widgets/custom_loader.dart
+++ lib/widgets/custom_loader.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class CustomLoader extends StatelessWidget {
   const CustomLoader({super.key});
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
